36b4164f25514a870023c676b008759955dd8ea0
junichi0829 <junjundesuu@yahoo.co.jp>
2023-01-18 13:01:00 +0900

    First commit.

---
+++ DotNote_Backend/src/main/java/com/global/dotnote/DotNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.global.dotnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotNoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotNoteBackendApplication.class, args);
+	}
+
+}
---
+++ DotNote_Backend/src/test/java/com/global/dotnote/DotNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.dotnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotNoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
